f2a5f0a0c4a72f4e321846c758901a5e7f7b9aa9
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2023-04-28 11:55:45 +0330

    chore: refactor inputs into a resuable component

---
+++ components/Input.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form-control w-full relative" :class="containerClass">
+    <label :for="id" class="label">
+      <span class="label-text label-3 text-gray-500">{{ label }}</span>
+    </label>
+    <input
+      :id="id"
+      v-bind="$attrs"
+      class="input input-bordered input-primary w-full"
+      :class="inputClass"
+    />
+    <div v-if="$slots.startAdornment" class="absolute top-[50px] right-3">
+      <slot name="startAdornment" />
+    </div>
+    <div v-if="$slots.endAdornment" class="absolute top-[50px] left-3">
+      <slot name="endAdornment" />
+    </div>
+    <div v-if="$slots.helperText" class="mt-2">
+      <slot name="helperText" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  id: string;
+  label: string;
+  modelValue: string;
+  error?: boolean;
+  errorText?: string;
+  inputClass?: string;
+  containerClass?: string;
+};
+
+type Emits = {
+  (e: 'update:modelValue', value: string): void;
+};
+
+defineProps<Props>();
+defineEmits<Emits>();
+</script>
+
+<script lang="ts">
+export default defineComponent({
+  inheritAttrs: false,
+});
+</script>
+
+<style scoped></style>
--- pages/login.vue
+++ pages/login.vue
@@ -4,42 +4,47 @@
     <h1 class="my-4 text-primary-500 font-bold text-3xl">شمس</h1>
     <h4 class="mb-3 text-primary-500 headline-3">صندوق قرض الحسنه</h4>
     <h4 class="mb-12 text-primary-500 label-3">تأسیس ۱۳۸۲</h4>
-    <div class="form-control w-full mb-8 relative">
-      <label for="mobile" class="label">
-        <span class="label-text label-3 text-gray-500">شماره موبایل</span>
-      </label>
-      <input
-        type="tel"
-        id="mobile"
-        dir="ltr"
-        class="mb-2 pr-14 input input-bordered input-primary w-full"
-      />
-      <span dir="ltr" class="absolute top-[50px] left-3 text-gray-500">
-        +98 |
-      </span>
-      <p class="body-3 text-gray-500 text-start">
-        لطفا شماره همراه را بدون صفر اول وارد کنید
-      </p>
-    </div>
-    <div class="form-control w-full mb-12 relative">
-      <label for="password" class="label">
-        <span class="label-text label-3 text-gray-500">رمز عبور</span>
-      </label>
-      <input
-        type="password"
-        id="password"
-        dir="ltr"
-        class="input input-bordered input-primary w-full"
-      />
-      <Eye class="absolute top-[50px] right-3" />
-    </div>
+    <Input
+      v-model="phone"
+      id="mobile"
+      label="شماره موبایل"
+      placeholder="9121234567"
+      dir="ltr"
+      type="tel"
+      inputClass="pr-14"
+      containerClass="mb-8"
+    >
+      <template #endAdornment>
+        <span dir="ltr" class="text-gray-500"> +98 | </span>
+      </template>
+      <template #helperText>
+        <p class="body-3 text-gray-500 text-start">
+          لطفا شماره همراه را بدون صفر اول وارد کنید
+        </p>
+      </template>
+    </Input>
+    <Input
+      v-model="password"
+      id="password"
+      label="رمز عبور"
+      dir="ltr"
+      type="password"
+      containerClass="mb-12"
+    >
+      <template #startAdornment>
+        <Eye />
+      </template>
+    </Input>
     <button type="submit" class="btn btn-primary btn-block label-1">
       ورود
     </button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const phone = ref('');
+const password = ref('');
+</script>
 
 <style scoped>
 .top-sheet {
