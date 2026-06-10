a6616193f3996a805376911b46307a8ab3000aec
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2026-06-11 00:21:22 +0200

    update externals again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     },
     bundledStorage: [],
     externals: {
-      inline: ['./server/generated/prisma'],
+      inline: [/generated\/prisma/],
     },
   },
   ssr: false,
