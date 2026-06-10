536642aca339c11728ccc82ba7341f7d3b7b6213
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2026-06-11 00:12:53 +0200

    move prisma out put to server folder

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from './generated/prisma';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 export const prisma = new PrismaClient({
